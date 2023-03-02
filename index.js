028955e5e7358a718ef1b05241859ace301914e0
shelby <shelbynarnold@yahoo.com>
2023-03-02 11:06:38 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => console.log(renderBooks(data)))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
